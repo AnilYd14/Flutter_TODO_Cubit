2f42f8be01e9219603cb729d3550d4a8ed612137
Anil Yadav <anil.yadav.ttn@hyke.ai>
2023-03-14 10:08:56 +0530

    update file

--- lib/task_screen.dart
+++ lib/task_screen.dart
@@ -1,62 +1,64 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todo_cubit/bloc/task_state.dart';
-import 'package:todo_cubit/task.dart';
 import 'package:todo_cubit/tasks_list.dart';
 
 import 'add_task_screen.dart';
 import 'bloc/task_cubit.dart';
 
-
 class TaskScreen extends StatelessWidget {
-
-
   final List<String> tasks = [];
 
-   TaskScreen({super.key});
-
+  TaskScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-      return Scaffold(
+    return Scaffold(
       backgroundColor: Colors.blueAccent,
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.lightBlueAccent,
         onPressed: () {
-          showModalBottomSheet(context: context, builder: (context)=>AddTaskScreen((newTextTile){
-            BlocProvider.of<TaskCubit>(context).addTask(newTextTile);
+          showModalBottomSheet(
+              context: context,
+              builder: (context) => AddTaskScreen((newTextTile) {
+                    BlocProvider.of<TaskCubit>(context).addTask(newTextTile);
 
-            Navigator.pop(context);
-          }));
+                    Navigator.pop(context);
+                  }));
         },
         child: const Icon(Icons.add),
       ),
-      body: Container(padding: const EdgeInsets.only(left: 30.0,right: 30.0,top: 60.0,bottom: 30.0),
+      body: Container(
+        padding: const EdgeInsets.only(
+            left: 30.0, right: 30.0, top: 60.0, bottom: 30.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children:  [
-            CircleAvatar(
+          children: [
+            const CircleAvatar(
               child: Icon(Icons.list),
               backgroundColor: Colors.white,
             ),
-            SizedBox(height: 20),
-            Text("TODO",style: TextStyle(fontSize: 30,color: Colors.white),),
-            Text('${context.watch<TaskCubit>().state.allTasks.length} Tasks', style: TextStyle(fontSize: 20, color: Colors.white)),
-            BlocBuilder<TaskCubit,TaskState>(
-              builder: (context,state) {
-                return Expanded(
-                  child: Container(
-                    height: 300.0,
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(20.0),
-                            topRight: Radius.circular(20.0))),
-                    child: TaskList(state.allTasks),
-                  ),
-                );
-              }
-            )
+            const SizedBox(height: 20),
+            const Text(
+              "TODO",
+              style: TextStyle(fontSize: 30, color: Colors.white),
+            ),
+            Text('${context.watch<TaskCubit>().state.allTasks.length} Tasks',
+                style: const TextStyle(fontSize: 20, color: Colors.white)),
+            const SizedBox(height: 20),
+            BlocBuilder<TaskCubit, TaskState>(builder: (context, state) {
+              return Expanded(
+                child: Container(
+                  height: 300.0,
+                  decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(20.0),
+                          topRight: Radius.circular(20.0))),
+                  child: TaskList(state.allTasks),
+                ),
+              );
+            })
           ],
         ),
       ),
